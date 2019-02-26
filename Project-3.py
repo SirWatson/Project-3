2cbd299c7fc38571612be3fbef935e78b52d5a05
SirWatson <danielwatson11@gmail.com>
2019-02-26 09:01:11 -0600

    Added monthly counters

--- Project-3.py
+++ Project-3.py
@@ -31,7 +31,8 @@ def main():
     #print(response.read())
     #data = response.read()
     #print(data)
-
+    data = []
+    count = 0
     #Open file
     #Parse file
     with urllib.request.urlopen("https://s3.amazonaws.com/tcmg476/http_access_log") as response:
@@ -42,12 +43,65 @@ def main():
     #html = html.replace("\\n", " ")
     #print(html)
     regex = r"(local|remote) - - \[(\d+)\/(\w+)\/(\d+)(.*?)] (.*?) (.*?) (.*?) (\d+) (\d+)"
+    regex2 = r"(.*?)\'(local|remote) - - \[(\d+)\/(\w+)\/(\d+)(.*?)] (.*?) (.*?) (.*?) (\d+) (\d+)"
     #re.compile(regex) 
     #regex = re.compile("^.+- - \[(\d+)\/(\w+)\/(\d+).+] \"(\w+) (.*) .+? (.*?)")
     #print(response.read())
+    Jan = 0
+    Feb = 0
+    Mar = 0
+    Apr = 0
+    May = 0
+    Jun = 0
+    Jul = 0
+    Aug = 0
+    Sep = 0
+    Oct = 0
+    Nov = 0
+    Dec = 0 
+    Months = [Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec]
+
     for match in re.finditer(regex, html):
-        print(match)
-    data = re.split(regex, str(html))
+        data.append(match)
+        if (match.group(3) == "Jan"):
+            Jan += 1
+        if (match.group(3) == "Feb"):
+            Feb += 1
+        if (match.group(3) == "Mar"):
+            Mar += 1
+        if (match.group(3) == "Apr"):
+            Apr += 1
+        if (match.group(3) == "May"):
+            Months[May] += 1
+        if (match.group(3) == "Jun"):
+            Months[Jun] += 1
+        if (match.group(3) == "Jul"):
+            Months[Jul] += 1
+        if (match.group(3) == "Aug"):
+            Months[Aug] += 1
+        if (match.group(3) == "Sep"):
+            Months[Sep] += 1
+        if (match.group(3) == "Oct"):
+            Months[Oct] += 1
+        if (match.group(3) == "Nov"):
+            Months[Nov] += 1
+        if (match.group(3) == "Dec"):
+            Months[Dec] += 1
+        #print(match)
+    print(Jan)
+    print(Feb)
+    print(Mar)
+    # print("okay")
+
+    # for i in range(len(data)):
+    #     look = re.search(regex2, str(data[i]))
+    #     print(data[i])
+    #     #print(look)
+    #     if look:
+    #         print(data[i].group(4))
+            
+    #print(data[2])
+    #data = re.split(regex, str(html))
     #print(data.group(15))
     #logfile = re.match(regex, str(html))
     #print(logfile)
@@ -55,12 +109,11 @@ def main():
         #print(logfile.group(1))
     #print(len(data))
     #print(data)
-    Oct = 0
-    Months = [Oct]
+    
     #count = 0
-    print(data[1])
-    # for i in range (len(data)):
-    #     print(data[i])
+    #print(data[1])
+    #for i in range (len(data)):
+        #print(data[i])
     #     target = re.search(regex, data[i])
     #     if target:
     #         print("Howdy")
